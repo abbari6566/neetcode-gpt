325721d221b0ca484e9c5f1ce326d086901c8748
Abrar F. Bari <barifaiazabrar@gmail.com>
2026-06-21 17:34:20 -0500

    Update Activation functions (Math Foundations 2/6)

--- foundations/activations.py
+++ foundations/activations.py
@@ -1,15 +1,24 @@
-class Solution:
-    def get_minimizer(self, iterations: int, learning_rate: float, init: int) -> float:
-        # Objective function: f(x) = x^2
-        # Derivative:         f'(x) = 2x
-        # Update rule:        x = x - learning_rate * f'(x)
-        # Round final answer to 5 decimal places
-        x_old = init
-        x_new = 0
-        if(iterations == 0):
-            return x_old
-        for _ in range(iterations):
-            x_new = x_old - (learning_rate * 2*(x_old))
-            x_old = x_new
-        return round(x_new, 5)
+import numpy as np
+from numpy.typing import NDArray
 
+
+class Solution:
+    
+    def sigmoid(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        # z is a 1D NumPy array
+        # Formula: 1 / (1 + e^(-z))
+        # return np.round(your_answer, 5)
+
+        # result = []
+        # for i in range(len(z)):
+        #     Z = 1/(1 + math.exp(-z[i]))
+        #     result.append(np.round(Z,5))
+        
+        # instead of for loop use numpy np that can simultaneously calculate element wise
+        ans = 1 / (1 + np.exp(-z))
+        return np.round(ans, 5)
+
+    def relu(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        # z is a 1D NumPy array
+        # Formula: max(0, z) element-wise
+        return np.maximum(0, z).round(2)
