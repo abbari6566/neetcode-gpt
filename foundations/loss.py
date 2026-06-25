2f201e42755a41ebc300a5b2c328e5353bca5485
Abrar F. Bari <barifaiazabrar@gmail.com>
2026-06-25 18:45:30 -0500

    Implement Cross-entropy loss (Math Foundations 4/6)

---
+++ foundations/loss.py
@@ -0,0 +1,32 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Solution:
+
+    def binary_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
+        # y_true: true labels (0 or 1)
+        # y_pred: predicted probabilities
+        # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
+        # return round(your_answer, 4)
+        epsilon = 1e-7
+        y_pred = np.clip(y_pred, epsilon, 1-epsilon)
+        n=len(y_pred)
+        avg_loss = (-1/n)*np.sum((y_true*np.log(y_pred) + (1-y_true)* np.log(1-y_pred)))
+        return np.round(avg_loss,4)
+
+        
+
+    def categorical_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
+        # y_true: one-hot encoded true labels (shape: n_samples x n_classes)
+        # y_pred: predicted probabilities (shape: n_samples x n_classes)
+        # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
+        # return round(your_answer, 4)
+
+        epsilon = 1e-7
+        y_pred = np.clip(y_pred, epsilon, 1-epsilon)
+        #get the number of samples (rows)
+        n = y_pred.shape[0]
+        #sum up the element wise multiplication of y_true and log(y_pred)
+        avg_loss = (-1/n) * np.sum(y_true * np.log(y_pred))
+        return np.round(avg_loss, 4)
